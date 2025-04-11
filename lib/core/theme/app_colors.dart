2f0d636d5f13aaa1ed85e135d1d65894c4f42aa7
AtharvaThite <authite@gmail.com>
2025-04-11 21:12:44 +0530

    Added AppColors

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFD5C1A6);
+  static const Color secondaryColor = Color(0xFFFFF3E7);
+  static const Color fontColor = Color(0xFF0A1A4B);
+  static const Color borderColor = Color(0xFF202939);
+}
