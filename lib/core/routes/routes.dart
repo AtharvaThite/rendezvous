96db7b5715e1e375ff31ead2fa589abcd81477d9
AtharvaThite <authite@gmail.com>
2025-04-12 23:53:33 +0530

    Refactor onboarding route to set initial page index based on email verification and profile completion status

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:rendezvous/core/routes/route_names.dart';
 import 'package:rendezvous/core/services/dependacy_injection.dart';
+import 'package:rendezvous/core/utils/shared_prefs_keys.dart';
 import 'package:rendezvous/src/onboarding/presentation/providers/email_verification_provider.dart';
 import 'package:rendezvous/src/onboarding/presentation/providers/onboarding_state_manager.dart';
 import 'package:rendezvous/src/onboarding/presentation/views/onboarding_screen.dart';
@@ -11,20 +12,53 @@ import 'package:shared_preferences/shared_preferences.dart'
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case RouteNames.onboardingScreen:
-      return _pageRouteBuilder(
-        (context) => MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-              create: (context) => sl<EmailVerificationProvider>(),
-            ),
-            ChangeNotifierProvider(
-              create: (context) => OnboardingStateManager(),
-            ),
-          ],
-          child: const OnboardingScreen(),
-        ),
-        settings: settings,
-      );
+      final prefs = sl<SharedPreferences>();
+      final isEmailVerified =
+          prefs.getBool(SharedPrefsKeys.isEmailVerified) ?? false;
+      final isProfileCompleted =
+          prefs.getBool(SharedPrefsKeys.isProfileCompleted) ?? false;
+
+      // log('isEmailVerified = $isEmailVerified');
+      // log('isProfileCompleted = $isProfileCompleted');
+      return _pageRouteBuilder((context) {
+        if (!isEmailVerified) {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (context) => sl<EmailVerificationProvider>(),
+              ),
+              ChangeNotifierProvider(
+                create: (context) => OnboardingStateManager()..setPageIndex(0),
+              ),
+            ],
+            child: const OnboardingScreen(),
+          );
+        } else if (!isProfileCompleted && isEmailVerified) {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (context) => sl<EmailVerificationProvider>(),
+              ),
+              ChangeNotifierProvider(
+                create: (context) => OnboardingStateManager()..setPageIndex(2),
+              ),
+            ],
+            child: const OnboardingScreen(),
+          );
+        } else {
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (context) => sl<EmailVerificationProvider>(),
+              ),
+              ChangeNotifierProvider(
+                create: (context) => OnboardingStateManager()..setPageIndex(0),
+              ),
+            ],
+            child: const OnboardingScreen(),
+          );
+        }
+      }, settings: settings);
 
     // Route to Page Not Found (Default) Page
     default:
