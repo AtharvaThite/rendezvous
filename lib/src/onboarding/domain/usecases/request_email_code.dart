4dc6d73451be2fd5deb9275f49b2838d44c67e10
AtharvaThite <authite@gmail.com>
2025-04-12 21:22:01 +0530

    Refactor RequestEmailCode use case to return a Future<String> instead of void

--- lib/src/onboarding/domain/usecases/request_email_code.dart
+++ lib/src/onboarding/domain/usecases/request_email_code.dart
@@ -2,11 +2,12 @@ import 'package:rendezvous/core/usecase/usecase.dart';
 import 'package:rendezvous/core/utils/typedefs.dart';
 import 'package:rendezvous/src/onboarding/domain/repo/onboarding_repo.dart';
 
-class RequestEmailCode extends UsecaseWithParams<void, String> {
+class RequestEmailCode extends UsecaseWithParams<String, String> {
   const RequestEmailCode(this._repo);
 
   final OnboardingRepo _repo;
 
   @override
-  ResultVoid call(String email) async => _repo.requestEmailCode(email: email);
+  ResultFuture<String> call(String email) async =>
+      _repo.requestEmailCode(email: email);
 }
