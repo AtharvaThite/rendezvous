e487c4939a8dcc4bcd574eeee3ab8067ac5c77d9
AtharvaThite <authite@gmail.com>
2025-04-12 09:58:00 +0530

    Add error color to AppColors class

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color secondaryColor = Color(0xFFFFF3E7);
   static const Color fontColor = Color(0xFF0A1A4B);
   static const Color borderColor = Color(0xFF202939);
+  static const Color error = Color(0xFFFA5A4E);
 }
