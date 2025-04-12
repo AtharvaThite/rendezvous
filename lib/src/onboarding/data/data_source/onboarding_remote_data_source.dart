e55f8ca0e3a40d8cca66f49b5be628146c6c120f
AtharvaThite <authite@gmail.com>
2025-04-12 23:55:02 +0530

    Refactor verifyEmailCode method to accept email parameter and improve error handling in OnboardingRemoteDataSource

--- lib/src/onboarding/data/data_source/onboarding_remote_data_source.dart
+++ lib/src/onboarding/data/data_source/onboarding_remote_data_source.dart
@@ -4,13 +4,14 @@ import 'dart:developer';
 import 'package:http/http.dart' as http;
 import 'package:rendezvous/core/errors/exceptions.dart';
 import 'package:rendezvous/core/utils/api_urls.dart';
+import 'package:rendezvous/core/utils/shared_prefs_keys.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 abstract class OnboardingRemoteDataSource {
   const OnboardingRemoteDataSource();
 
   Future<String> requestEmailCode({required String email});
-  Future<void> verifyEmailCode({required int code});
+  Future<void> verifyEmailCode({required String email, required String code});
 }
 
 class OnboardingRemoteDataSourceImpl extends OnboardingRemoteDataSource {
@@ -22,26 +23,25 @@ class OnboardingRemoteDataSourceImpl extends OnboardingRemoteDataSource {
   @override
   Future<String> requestEmailCode({required String email}) async {
     try {
+      await _prefs.setString(SharedPrefsKeys.email, email);
       final response = await _client.post(
         Uri.parse(ApiUrls.requestEmailCode),
         headers: {'Content-Type': 'application/json'},
         body: jsonEncode({'email': email}),
       );
+      final responseBody = jsonDecode(response.body) as Map<String, dynamic>;
 
       if (response.statusCode == 200) {
-        log('Email code requested successfully');
-        log(response.body);
-        final responseBody = jsonDecode(response.body) as Map<String, dynamic>;
         return responseBody['message'] as String;
       } else {
-        log('Failed with status: ${response.statusCode}');
         throw APIException(
           statusCode: response.statusCode,
-          message: response.body,
+          message: responseBody['error'] as String,
         );
       }
-    } catch (e, s) {
-      log('Exception: $e', stackTrace: s);
+    } on APIException {
+      rethrow;
+    } catch (e) {
       throw const APIException(
         statusCode: 800,
         message: 'Something went wrong',
@@ -50,24 +50,32 @@ class OnboardingRemoteDataSourceImpl extends OnboardingRemoteDataSource {
   }
 
   @override
-  Future<void> verifyEmailCode({required int code}) async {
+  Future<void> verifyEmailCode({
+    required String email,
+    required String code,
+  }) async {
     try {
       final response = await _client.post(
-        Uri.parse(ApiUrls.requestEmailCode),
+        Uri.parse(ApiUrls.verifyEmailCode),
         headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'code': code}),
+        body: jsonEncode({'email': email,'code': code}),
       );
+      final responseBody = jsonDecode(response.body) as Map<String, dynamic>;
 
       if (response.statusCode == 200) {
         log('Email code verified');
+        await _prefs.setBool(SharedPrefsKeys.isEmailVerified, true);
+
         return;
       } else {
         log('Verification failed: ${response.statusCode}');
         throw APIException(
           statusCode: response.statusCode,
-          message: response.body,
+          message: responseBody['error'] as String,
         );
       }
+    } on APIException {
+      rethrow;
     } catch (e, s) {
       log('Exception: $e', stackTrace: s);
       throw const APIException(
