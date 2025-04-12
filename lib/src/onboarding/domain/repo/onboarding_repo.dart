8937623b7e075589cd36933445502b85220a6ed0
AtharvaThite <authite@gmail.com>
2025-04-12 21:22:24 +0530

    Refactor OnboardingRepo to change requestEmailCode return type from ResultVoid to ResultFuture<String>

--- lib/src/onboarding/domain/repo/onboarding_repo.dart
+++ lib/src/onboarding/domain/repo/onboarding_repo.dart
@@ -3,6 +3,6 @@ import 'package:rendezvous/core/utils/typedefs.dart';
 abstract class OnboardingRepo {
   const OnboardingRepo();
 
-  ResultVoid requestEmailCode({required String email});
+  ResultFuture<String> requestEmailCode({required String email});
   ResultFuture<void> verifyEmailCode({required int code});
 }
