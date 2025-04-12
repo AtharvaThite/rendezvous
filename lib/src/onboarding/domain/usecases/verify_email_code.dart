7bc5cb9d1882614ad416a6d0b3ae905b6e9fcdbf
AtharvaThite <authite@gmail.com>
2025-04-12 23:55:20 +0530

    Refactor VerifyEmailCode use case to accept VerifyCodeParams and improve parameter handling

--- lib/src/onboarding/domain/usecases/verify_email_code.dart
+++ lib/src/onboarding/domain/usecases/verify_email_code.dart
@@ -1,12 +1,24 @@
+import 'package:equatable/equatable.dart';
 import 'package:rendezvous/core/usecase/usecase.dart';
 import 'package:rendezvous/core/utils/typedefs.dart';
 import 'package:rendezvous/src/onboarding/domain/repo/onboarding_repo.dart';
 
-class VerifyEmailCode extends UsecaseWithParams<void, int> {
+class VerifyEmailCode extends UsecaseWithParams<void, VerifyCodeParams> {
   const VerifyEmailCode(this._repo);
 
   final OnboardingRepo _repo;
 
   @override
-  ResultFuture<void> call(int code) async => _repo.verifyEmailCode(code: code);
+  ResultFuture<void> call(VerifyCodeParams params) async =>
+      _repo.verifyEmailCode(email: params.email, code: params.code);
+}
+
+class VerifyCodeParams extends Equatable {
+  const VerifyCodeParams({required this.code, required this.email});
+  const VerifyCodeParams.empty() : this(email: '', code: '');
+  final String email;
+  final String code;
+
+  @override
+  List<String> get props => [email, code];
 }
