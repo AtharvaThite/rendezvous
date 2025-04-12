3178c0042d14a730307ac92f6700074bce7df49d
AtharvaThite <authite@gmail.com>
2025-04-12 23:49:55 +0530

    Add SharedPrefsKeys class for managing shared preferences keys

---
+++ lib/core/utils/shared_prefs_keys.dart
@@ -0,0 +1,6 @@
+class SharedPrefsKeys {
+  static const String email = 'userEmail';
+  static const String isEmailVerified = 'isEmailVerified';
+  static const String isProfileCompleted = 'isProfileCompleted';
+  static const String isProfileApproved = 'isProfileApproved';
+}
